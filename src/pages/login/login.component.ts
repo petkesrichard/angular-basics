1be4ac3ad8afb3761c119588e390d1389a33cdf3
petkes <petkesrichardadnras@gmail.com>
2017-08-30 08:33:24 +0300

    login view

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -5,7 +5,8 @@ import {Router} from "@angular/router";
 
 @Component({
     selector: 'login',
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 
 
